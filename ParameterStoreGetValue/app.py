fe2f41d2692adf31623cf75263d971f47babf735
Andrew Rodko <rudmaster667@gmail.com>
2023-12-13 16:04:02 -0500

    Remove comment test

--- ParameterStoreGetValue/app.py
+++ ParameterStoreGetValue/app.py
@@ -4,7 +4,6 @@ from urllib.request import Request, urlopen
 
 AWS_SESSION_TOKEN = os.environ.get('AWS_SESSION_TOKEN')
 PARAMETER_STORE_URL = 'http://localhost:2773/systemsmanager/parameters/get?name={parameter_name}&withDecryption=true'
-# comment added to verify Codpipeline functionality
 
 
 def get_parameter_name(event):
