cf2ba15bb60030ba3a9a45758ea49a3fe60360ff
Andrew Rodko <rudmaster667@gmail.com>
2023-12-13 15:53:55 -0500

    Add unit testing

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,14 +1,14 @@
 import json
-import pytest
+import unittest
 
 from ParameterStoreGetValue import app
+class TestHandlerCase(unittest.TestCase):
+    def test_response(self):
+        print("testing response")
+        event = {"Name": "/sam/vpcProd"}
+        result = app.lambda_handler(event, None)
+        print(result)
+        self.assertEqual(result['body'], "vpc-0d6712c4bd670aca6")
 
-
-@pytest.fixture()
-def fixture_event():
-    return {"Name": "/sam/vpcProd"}
-
-class TestParameterStoreGetValue:
-    def test_get_parameter_value(self, fixture_event):
-        assert app.get_parameter_value(fixture_event) == "vpc-0d6712c4bd670aca6"
-
+if __name__ == '__main__':
+    unittest.main()
