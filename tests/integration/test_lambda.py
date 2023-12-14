62c245a4ba0ae74e09c6e5b82d5eabcc1c55445e
Andrew Rodko <rudmaster667@gmail.com>
2023-12-13 21:24:47 -0500

    Add unit and integration testing

---
+++ tests/integration/test_lambda.py
@@ -0,0 +1,21 @@
+import json
+import unittest
+import boto3
+
+from ParameterStoreGetValue import app
+
+
+class TestHandlerCase(unittest.TestCase):
+    def test_response(self):
+        print("testing response")
+        event = {"Name": "/sam/vpcProd"}
+        lambda_client = boto3.client('lambda')
+        lambda_response = lambda_client.invoke(FunctionName="ParameterStoreGetValueFunction_DEV",
+                                               InvocationType='RequestResponse', Payload=json.dumps(event))
+        result = json.load(lambda_response['Payload'])
+        print(result)
+        self.assertEqual(result['body'], "vpc-0d6712c4bd670aca6")
+
+
+if __name__ == '__main__':
+    unittest.main()
